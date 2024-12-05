ad4ae916806fbbfbda4f3f909122b2866aa4868e
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-05 16:39:07 -0500

    Update `use@` directives and change them back after compilation

--- cxlc.py
+++ cxlc.py
@@ -16,9 +16,9 @@ with open(argv[1], 'r') as f:
 
 for i in str_w:
   if (i.startswith("use")):
-    module = i.replace("use@", "").replace("*!", "")
+    module = i.replace("use@", "")
     f_module = module.replace("\n", "")
-    tp_file.append(i.replace(f_module, "").replace("use@", "#include \"" + f_module + ".cxlbund/" + f_module + ".h").replace("*!", "\""))
+    tp_file.append(i.replace(f_module, "").replace("use@", "#include \"" + f_module + ".cxlbund/" + f_module + ".h").replace("\n", "\"\n"))
   else:
     code.append(i)
 
@@ -34,7 +34,7 @@ whatf = []
 
 for i in str_w:
   if (i.startswith("use")):        
-    module = i.replace("use@", "").replace("*!", "")
+    module = i.replace("use@", "")
     whatf.append(module.strip())
 
 """
@@ -50,3 +50,27 @@ compre += argv[1] + " -o " + filename
 print(compre)
 
 os.system(compre + " -std=c++17")
+
+"""
+change back
+"""
+
+includes = []
+includes_rep = []
+code_w = []
+
+with open(argv[1], 'r') as f:
+  w_ph = f.readlines()
+  for i in w_ph:
+    if (i.startswith("#include")): # io.cxlbund/io.h
+      inc_ = i.replace("#include \"", "").replace("\"", "")
+      inc = re.sub(".cxlbund.*", "", inc_)
+      includes.append("use@" + inc)
+    else:
+      code_w.append(i)
+
+with open(argv[1], 'w') as f:
+  for i in includes:
+    f.write(i)
+  for i in code_w:
+    f.write(i)
