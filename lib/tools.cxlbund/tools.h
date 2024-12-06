5e7a06ebe0bb60308a9f2931322e495677ba6869
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-05 20:55:53 -0500

    Add split function to tools.h

--- lib/tools.cxlbund/tools.h
+++ lib/tools.cxlbund/tools.h
@@ -1,2 +1,4 @@
 #include <iostream>
+#include <vector>
 std::string remins(std::string subject, std::string what, int times);
+std::vector<std::string> split(std::string subject, std::string delim, int times);
