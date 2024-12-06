30d3ce4d71c39c0eccf790bc318f362a28d1fdc6
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-05 20:56:13 -0500

    Add split function to tools

--- lib/tools.cxlbund/tools.cpp
+++ lib/tools.cxlbund/tools.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "tools.h"
 using namespace std;
 
@@ -9,4 +11,20 @@ string remins(string subject, string what, int times) {
         subject.erase(index, len);
     }
     return subject;
+}
+
+vector<string> split(string subject, string delim, int times) {
+    vector<string> ret;
+    string b_sub = subject;
+    for (int i = 0; i <= times; i++) {
+        int index = b_sub.find(delim);
+        if (index < subject.size()) {
+            ret.push_back(b_sub.substr(0, index));
+            b_sub.erase(0, index+1);
+        } else {
+            ret.push_back(b_sub);
+            return ret;
+        }
+    }
+    return ret;
 }
