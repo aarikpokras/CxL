0cf1e3d2ca8c8b3fc3881a0bffaa65f2093d37a4
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-07 15:07:19 -0500

    Add `#pragma once`

--- lib/tools.cxlbund/tools.h
+++ lib/tools.cxlbund/tools.h
@@ -1,4 +1,5 @@
 #include <iostream>
 #include <vector>
+#pragma once
 std::string remins(std::string subject, std::string what, int times);
 std::vector<std::string> split(std::string subject, std::string delim, int times);
