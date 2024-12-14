618667079923fc16825b63293d5aeee5dc52db67
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-13 23:30:47 -0500

    Update tools.h

--- lib/tools.cxlbund/tools.h
+++ lib/tools.cxlbund/tools.h
@@ -3,4 +3,5 @@
 #include <vector>
 #pragma once
 std::string remins(std::string subject, std::string what, int times);
-std::vector<std::string> split(std::string subject, std::string delim, int times);
+std::vector<std::string> split(std::string subject, std::string delim,
+                               int times);
