eddf72c352969d7f260d78435ad49641bdea1ceb
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-14 15:32:07 -0500

    `move_file` function

--- lib/shell.cxlbund/shell.c
+++ lib/shell.cxlbund/shell.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "shell.h"
 
 int execute(const char *command) {
   return system(command);
+}
+
+int move_file(const char* origin, const char* destination) {
+  char warn[512];
+  snprintf(warn, 512, "This program is trying to change file %s. Use ^C now if you wish to stop this.", origin);
+  printf("%s\n", warn);
+  sleep(1);
+  char buf[256];
+  snprintf(buf, 256, "mv \"%s\" \"%s\"", origin, destination);
+  return system(buf);
 }
