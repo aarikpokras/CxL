daf9c7ec1a164ed3e80397b630f6c7f22e546f14
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-15 12:24:16 -0500

    `file_size` function

---
+++ lib/fs.cxlbund/fs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int file_size(const char* file) {
+  FILE *f = fopen(file, "r");
+  if (f == NULL) {
+    return 0;
+  }
+  fseek(f, 0, SEEK_END);
+  int ret_ = ftell(f);
+  rewind(f);
+  fclose(f);
+  return ret_;
+}
