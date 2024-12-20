ca29179adee0f2d19bc64c7f566688fd3499ae9c
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-12-20 11:07:42 -0500

    Impl. for `reverse`

--- lib/tools.cxlbund/tools.h
+++ lib/tools.cxlbund/tools.h
@@ -5,3 +5,4 @@
 std::string remins(std::string subject, std::string what, int times);
 std::vector<std::string> split(std::string subject, std::string delim,
                                int times);
+std::string reverse(std::string subject);
